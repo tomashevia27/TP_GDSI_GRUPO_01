55a244119fc98216785cb77f295dbe5047884aef
Tomás Hevia <tomashevia27@gmail.com>
2026-05-10 18:17:23 -0300

    Create schemas.py
    cree 'schemas.py' para validar los datos que entran y salen de la API, para las tres user stories iniciales.

---
+++ schemas.py
@@ -0,0 +1,46 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+
+# -----------------------------------------
+# US 1: Registro de Usuario
+# -----------------------------------------
+class UsuarioRegistro(BaseModel):
+    nombre: str = Field(..., min_length=1, description="El nombre no puede estar vacío")
+    apellido: str = Field(..., min_length=1, description="El apellido no puede estar vacío")
+    email: EmailStr # valida el formato usuario@dominio.com automáticamente
+    password: str = Field(..., min_length=8, description="La contraseña debe tener al menos 8 caracteres")
+    edad: int
+    genero: str
+    zona: str
+    foto_perfil: Optional[str] = None
+
+# -----------------------------------------
+# US 2: Inicio de Sesión
+# -----------------------------------------
+class UsuarioLogin(BaseModel):
+    email: EmailStr
+    password: str = Field(..., min_length=1)
+
+# -----------------------------------------
+# US 3: Editar Perfil
+# -----------------------------------------
+class UsuarioEdicion(BaseModel):
+    nombre: str = Field(..., min_length=1)
+    apellido: str = Field(..., min_length=1)
+    edad: int
+    genero: str
+    password: str = Field(..., min_length=8)
+    zona: str
+    foto_perfil: Optional[str] = None
+
+# -----------------------------------------
+# Esquema de Respuesta (Lo que le devolvemos al frontend)
+# -----------------------------------------
+class UsuarioRespuesta(BaseModel):
+    id: int
+    nombre: str
+    apellido: str
+    email: EmailStr
+    
+    class Config:
+        orm_mode = True
